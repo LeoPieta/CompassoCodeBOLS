b0ff5ad8fb0565927296ce287093b3b6e9c8c257
Leonardo Pieta Monteiro <58449041+LeoPieta@users.noreply.github.com>
2020-02-26 17:31:40 -0300

    alteracao na class dinheiro no pack Brasileirice

--- brasileirice/src/main/java/br/com/alura/Dinheiro.java
+++ brasileirice/src/main/java/br/com/alura/Dinheiro.java
@@ -3,8 +3,13 @@ package br.com.alura;
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import javax.money.MonetaryOperator;
 
 import org.javamoney.moneta.Money;
+import org.javamoney.moneta.function.MonetaryOperators;
+
+import br.com.caelum.stella.inwords.FormatoDeReal;
+import br.com.caelum.stella.inwords.NumericToWordsConverter;
 
 public class Dinheiro {
 	public static void main(String[] args) {
@@ -13,5 +18,10 @@ public class Dinheiro {
 		System.out.println(valorDaParcela);
 		MonetaryAmount valorTotal = valorDaParcela.multiply(12);
 		System.out.println(valorTotal);
+		MonetaryAmount desconto = valorTotal.with(MonetaryOperators.percent(10));
+		System.out.println(desconto);
+		NumericToWordsConverter conversor = new NumericToWordsConverter(new FormatoDeReal());
+		System.out.println("Ola Aluno, ganhe " + conversor.toWords(desconto.getNumber().doubleValue()) + " por ouvir nosso podCast.");
+		
 	}
 }
