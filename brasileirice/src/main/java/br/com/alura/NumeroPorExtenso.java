e460382ec150cdc7078ac8076f47317e2d6e2eb1
Leonardo Pieta Monteiro <58449041+LeoPieta@users.noreply.github.com>
2020-02-26 15:35:31 -0300

    update na class NumeroPorExtenso no pack Brasileirice

--- brasileirice/src/main/java/br/com/alura/NumeroPorExtenso.java
+++ brasileirice/src/main/java/br/com/alura/NumeroPorExtenso.java
@@ -3,15 +3,16 @@ package br.com.alura;
 //import java.math.BigDecimal;
 import java.util.Scanner;
 
-import br.com.caelum.stella.inwords.InteiroSemFormato;
+import br.com.caelum.stella.inwords.FormatoDeReal;
+//import br.com.caelum.stella.inwords.InteiroSemFormato;
 import br.com.caelum.stella.inwords.NumericToWordsConverter;
 
 public class NumeroPorExtenso {
 	public static void main(String[] args) {
 		try {
-		NumericToWordsConverter conversor = new NumericToWordsConverter(new InteiroSemFormato());
+		NumericToWordsConverter conversor = new NumericToWordsConverter(new FormatoDeReal());
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Digite um numero e o sistema retornara o valor por extenso: ");
+		System.out.println("Digite um valor e o sistema retornara o valor por extenso: ");
 		System.out.println(conversor.toWords(scanner.nextDouble()));
 		
 //		BigDecimal valor = new BigDecimal(scanner.nextDouble());  // metodo utilizado no video da alura
