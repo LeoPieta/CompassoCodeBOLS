30ef1e7ff44c52e0c57149f6596c10ebfa59ac4b
Leonardo Pieta Monteiro <58449041+LeoPieta@users.noreply.github.com>
2020-03-17 15:00:34 -0300

    Update em novos arquivos

---
+++ Compassito/src/main/java/br/com/Compasso/Compassito/CompassitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.Compasso.Compassito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassitoApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassitoApplication.class, args);
+	}
+
+}
---
+++ CompassitoAPI/src/br/com/compasso/compassito/API_s.java
@@ -0,0 +1,8 @@
+package br.com.compasso.compassito;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class API_s {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("");
+}
